26be0777bdf30f52c6a44d24362af250506d4258
fauzanruslan25 <93021288+fauzanruslan25@users.noreply.github.com>
2022-03-28 18:33:14 +0800

    Create index.php

---
+++ crud/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Pendaftaran Siswa Baru Kursus </title>
+</head>
+<body>
+    <header>
+        <h3 style="text-align:center"> Pendaftaran Siswa baru </h3>
+        <h1 style="text-align:center"> Kursus Coding </h3>
+    </header>
+    <h4 style="text-align:center"> Menu </h4>
+    <nav>
+        <ul style="list-style-type:none;text-align:center;margin:0;padding:0">
+            <li stlye> <a href="form-daftar.php"> Daftar Menjadi Siswa baru   </a></li>
+            <li> <a href="list-siswa.php"> List Pendaftar </a> </li>
+        </ul>
+    </nav>
+
+    </body>
+</html>  

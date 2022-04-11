11516f4dc600d471a84051232e245685128b7527
fauzanruslan25 <93021288+fauzanruslan25@users.noreply.github.com>
2022-04-11 22:59:26 +0800

    Add files via upload

---
+++ Bootstrap/index.php
@@ -0,0 +1,65 @@
+<?php
+include("head.php")
+?>
+<div class="jumbotron text-center">
+    <h1>Belajar Programming Itu Mudah</h1>
+    <p>Tingkatkan skill programming kamu belajar tutorial pemrograman gratis!</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="thumbnail">
+                <a href="artikel.php"><img src="img/1.jpg" width="100%" alt="Gambar 1"></a>
+                <div class="caption">
+                    <h3>Belajar HTML & CSS</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris</p>
+                    <p><a href="artikel.php" class="btn btn-light btn-block" role="button">Selengkapnya</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="thumbnail">
+                <a href="slide2.php"><img src="img/2.jfif" width="100%" alt="Gambar 2"></a>
+                <div class="caption">
+                    <h3><a href="slide2.php" style="text-decoration:none;color: black;">Web Programming</a> </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris</p>
+                    <p><a href="slide2.php" class="btn btn-light btn-block" role="button">Selengkapnya</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="thumbnail">
+                <a href="slide3.php"><img src="img/4.jfif" width="100%" alt="Cinque Terre"></a>
+                <div class="caption">
+                    <h3>Belajar Python</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris</p>
+                    <p><a href="slide3.php" class="btn btn-light btn-block" role="button">Selengkapnya</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="thumbnail">
+                <a href="slide4.php"><img src="img/3.jpg"  width="100%" alt="Cinque Terre"></a>
+                <div class="caption">
+                    <h3>Desain UI & UX</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris</p>
+                    <p><a href="slide4.php" class="btn btn-light btn-block " role="button">Selengkapnya</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="jumbotron text-center" style="margin-bottom:0">
+    <p>Copyright 2020 Fauzan Ruslan</p>
+</div>
+</body>
+</html>
---
+++ Bootstrap/slide3.php
@@ -0,0 +1,134 @@
+<?php
+include("head.php")
+?>
+
+<div class="jumbotron text-center">
+    <h1>Belajar Python</h1>
+    <p>Tingkatkan skill programming kamu belajar tutorial pemrograman gratis!</p>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="thumbnail">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Pemrograman</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Belajar Python</li>
+                    </ol>
+                </nav>
+                <img src="img/4.jfif" width="100%" alt="Cinque Terre">
+                <div class="caption">
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris</p>
+                    <hr>
+                </div>
+                <div class="comment">
+                    <label><h2>Leave a coment</h2></label>
+                    <div class="form-group">
+                        <label>Name:</label>
+                        <input type="text" class="form-control" id="usr">
+                    </div>
+                    <div class="form-group">
+                        <label>Email:</label>
+                        <input type="email" class="form-control" id="pwd">
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Comment:</label>
+                        <textarea class="form-control" rows="5" id="comment"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-info" value="Kirim Komentar">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="caption">
+                        <h4>Web Programming</h4>
+                        <div class="row">
+                            <div class="col-xl-3">
+                                <img src="img/2.jfif" width="100%" alt="Cinque Terre">
+                            </div>
+                            <div class="col-sm-9">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisgna aliqua. adipisgna aliqua</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="caption">
+                        <h4>Belajar HTML & CSS</h4>
+                        <div class="row">
+                            <div class="col-xl-3">
+                                <img src="img/1.jpg" width="100%" alt="Cinque Terre">
+                            </div>
+                            <div class="col-sm-9">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisgna aliqua. adipisgna aliqua</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="caption">
+                        <h4>Desain UI & UX</h4>
+                        <div class="row">
+                            <div class="col-xl-3">
+                                <img src="img/3.jpg" width="100%" alt="Cinque Terre">
+                            </div>
+                            <div class="col-sm-9">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisgna aliqua. adipisgna aliqua</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="caption">
+                        <h4>Belajar Python</h4>
+                        <div class="row">
+                            <div class="col-xl-3">
+                                <img src="img/4.jfif" width="100%"alt="Cinque Terre">
+                            </div>
+                            <div class="col-sm-9">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisgna aliqua. adipisgna aliqua</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <img src="img/iklan.jpg" width="100%"alt="Cinque Terre">
+                </div>
+            </div>
+
+        </div>
+        </div>
+    </div>
+</div>
+<div class="jumbotron text-center" style="margin-bottom:0">
+    <p>Copyright 2020 Fauzan Ruslan</p>
+</div>
+</body>
+</html>

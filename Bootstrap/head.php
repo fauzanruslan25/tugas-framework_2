c8f3cd91e536b76dafcfac49aa4d0ea444489079
fauzanruslan25 <93021288+fauzanruslan25@users.noreply.github.com>
2022-04-11 22:46:14 +0800

    Create head.php

---
+++ Bootstrap/head.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Template Web Responsive dengan Bootstrap</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+	<script type="text/javascript" src="bootstrap.bundle.js"></script>
+	<script type="text/javascript" src="bootstrap.js"></script>
+	<script type="text/javascript" src="bootstrap.min.js"></script>
+	<script type="text/javascript" src="jquery.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-md bg-light navbar-light">
+    <!-- Brand -->
+    <a class="navbar-brand" href="index.php">
+        <img src="img/logouts.png" alt="logo" style="width:200px;">
+    </a>
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pemrograman</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Database</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Framework</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Lainnya</a>
+            </li>
+        </ul>
+    </div>
+</nav>
